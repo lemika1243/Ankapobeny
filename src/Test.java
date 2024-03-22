0509ab56385163f1b3854192041fbfd7839e8551
lemika1243 <lemikajy@gmail.com>
2024-03-22 08:27:42 +0300

    nety ilay izy ka

--- src/Test.java
+++ src/Test.java
@@ -8,7 +8,10 @@ public class Test {
     public static void main(String[] args) {
         try {
             MiDao myDao = new MiDao(null);
-            System.out.println(myDao.getAll(Fonenana.class));
+            for (int i = 0; i < 100; i++) {
+                System.out.println(myDao.getAll(Categorie.class));
+            }
+            myDao.closeConnection();
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
